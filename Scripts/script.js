6c1a62141b9928775135777d6ba37e5d9df65a5a
vaibagga <vaibagga@gmail.com>
2019-04-13 02:25:29 +0530

    privacy policy

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,7 @@
 console.log("Website loaded");
 console.log("2 + 2 is 4 quick maths");
 console.log("If you find any error report at https://bit.ly/IqT6zt")
+console.log(document.URL);
+console.log(document.body);
+console.log(document.links);
+console.log(document.head);
